1f4f0b2ab8fcfb7f74ec2ad1095fd44f71d9010a
therealjhay <92530531+therealjhay@users.noreply.github.com>
2025-06-29 19:05:35 +0100

    Update task_3.sql

--- task_3.sql
+++ task_3.sql
@@ -1,5 +1,7 @@
 -- task_3.sql
 -- Script to list all tables in the specified MySQL database.
--- The database name will be passed as an argument to the mysql command (e.g., mysql -u user -p alx_book_store < task_3.sql)
+-- This script is designed to be executed by passing the database name
+-- as an argument to the mysql command, like so:
+--    mysql -u your_user -p your_password alx_book_store < task_3.sql
 
 SHOW TABLES;
