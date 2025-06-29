5ae994fc3cf884a67d809db7169e4342f32d9f6e
therealjhay <92530531+therealjhay@users.noreply.github.com>
2025-06-29 19:04:19 +0100

    Create task_3.sql

---
+++ task_3.sql
@@ -0,0 +1,5 @@
+-- task_3.sql
+-- Script to list all tables in the specified MySQL database.
+-- The database name will be passed as an argument to the mysql command (e.g., mysql -u user -p alx_book_store < task_3.sql)
+
+SHOW TABLES;
